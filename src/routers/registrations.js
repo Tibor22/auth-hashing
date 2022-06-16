0225f1d01d1aebdc2a06712ea4d113c9a538cd31
Tibor22 <tothtibitravel@gmail.com>
2022-06-16 16:13:52 +0100

    finished

--- src/routers/registrations.js
+++ src/routers/registrations.js
@@ -1,21 +1,35 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const prisma = require('../utils/prisma.js')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const prisma = require("../utils/prisma.js");
 
-router.post('/', async (req, res) => {
-    const { username, password } = req.body;
+router.post("/", async (req, res) => {
+	console.log(req.body);
+	const { username, password } = req.body;
 
-    const createdUser = await prisma.user.create({
-        data: {
-            username,
-            password
+	try {
+		const passwordHash = await bcrypt.hash(password, 10);
+		const createdUser = await prisma.user.create({
+			data: {
+				username,
+				password: passwordHash,
+			},
+		});
+        if(!createdUser) {
+            throw new Error(`User ${username} already exist`)
         }
-    });
 
-    res.json({ data: createdUser });
+		return res.json({ data: createdUser });
+	} catch (e) {
+        if(e.code === "P2002") {
+            return res.status(400).json({error: 'Username already exist'})
+        }
+        console.log('WAGWAN');
+		
+	}
+
 });
 
 module.exports = router;
--- src/routers/sessions.js
+++ src/routers/sessions.js
@@ -1,26 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const prisma = require('../utils/prisma.js')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const prisma = require("../utils/prisma.js");
 
-router.post('/', async (req, res) => {
-    const { username, password } = req.body;
+const privateKey = process.env.JWT_SECRET;
 
-    const foundUser = await prisma.user.findFirst({
-        where: {
-            username
-        }
-    });
+router.post("/", async (req, res) => {
+	const { username, password } = req.body;
 
-    if (!foundUser || password !== foundUser.password) {
-        return res.status(401).json({ error: 'Invalid username or password.' });
-    }
+	const foundUser = await prisma.user.findFirst({
+		where: {
+			username,
+		},
+	});
+	if (!foundUser) {
+		return res.status(401).json({ error: "Invalid username." });
+	}
+	const comparedPassword = await bcrypt.compare(password, foundUser.password);
+	if (!foundUser || !comparedPassword) {
+		return res.status(401).json({ error: "Invalid username or password." });
+	}
 
-    const token = jwt.sign({ username }, process.env.JWT_SECRET);
+	console.log(foundUser);
+	const token = jwt.sign({ username }, privateKey);
 
-    res.json({ data: token });
+	res.json({ data: { token,username: foundUser.username}});
 });
 
 module.exports = router;
